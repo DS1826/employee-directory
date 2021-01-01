5943ddda35a175bdcc588c57998a85223260ace3
Dina Saunders <dina_saunders@mac.com>
2021-01-01 14:58:39 -0500

    added access point for Random User API

---
+++ src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// Export object containing methods we'll use for accessing Random User API
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    getUsers: function() {
+        return axios.get("https://randomuser.me/api/?results=50");
+    }
+};

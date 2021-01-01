daa1aa30333d28c1b7a333d5d506cb9da77613b3
Dina Saunders <dina_saunders@mac.com>
2021-01-01 15:29:25 -0500

    added and imported Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Wrapper from './components/Wrapper';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
-
+      <Header>
+      </Header>
       </Wrapper>
     </div>
   );

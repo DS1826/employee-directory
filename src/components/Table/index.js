52b10f7e7f23d186c005e65d97e3c440eec60d76
Dina Saunders <dina_saunders@mac.com>
2021-01-05 16:40:17 -0500

    added Table component to display employee data

---
+++ src/components/Table/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./style.css";
+
+const Table = (props) => {
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Photo</th>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.displayUsers[0] &&
+          props.displayUsers.map(user => {
+            return (
+              <tr>
+                <td><img src={user.picture.thumbnail}></img></td>
+                <td>{user.name.first} {user.name.last}</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;

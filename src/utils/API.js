f1b964d9fe355e5fc0913873e00df337101068cc
Dina Saunders <dina_saunders@mac.com>
2021-01-05 16:49:57 -0500

    updated API call to exclude non-US based users

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,6 +5,6 @@ import axios from "axios";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getUsers: function() {
-        return axios.get("https://randomuser.me/api/?results=50");
+        return axios.get("https://randomuser.me/api/?results=100&nat=US");
     }
 };

b2e7dad1d5b2df5787ddc3fcfff48040d61ea34a
Dina Saunders <dina_saunders@mac.com>
2021-01-05 16:50:32 -0500

    added alt property to img tag

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -18,7 +18,7 @@ const Table = (props) => {
           props.displayUsers.map(user => {
             return (
               <tr>
-                <td><img src={user.picture.thumbnail}></img></td>
+                <td><img src={user.picture.thumbnail} alt="employee"></img></td>
                 <td>{user.name.first} {user.name.last}</td>
                 <td>{user.phone}</td>
                 <td>{user.email}</td>

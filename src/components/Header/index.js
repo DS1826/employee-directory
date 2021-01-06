a78db958f8706bcb10a7f03fb611c10a0c46bf3e
Dina Saunders <dina_saunders@mac.com>
2021-01-06 09:31:02 -0500

    added search sort instructions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,7 @@ function Header() {
     return (
         <div className="header">
             <h1>Employee Directory</h1>
+            <p>Search or Sort by Employee Last Name</p>
         </div>
     );
 }
